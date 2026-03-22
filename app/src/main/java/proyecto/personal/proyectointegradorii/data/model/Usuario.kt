5c8df5ea461845b908cb9a62fa256a19a5810fe9
Dulce127 <20243ds105@utez.edu.mx>
2026-03-21 20:53:08 -0600

    Archivos creados y modificados para utilizar base de datos local :D

--- app/src/main/java/proyecto/personal/proyectointegradorii/data/model/Usuario.kt
+++ app/src/main/java/proyecto/personal/proyectointegradorii/data/model/Usuario.kt
@@ -1,7 +1,16 @@
 package proyecto.personal.proyectointegradorii.data.model
 
-data class Usuario (
-    val id: Int,
-    val nombreCompleto: String,
-    val correo: String
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "usuarios")
+data class Usuario(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val nombre_completo: String,
+    val correo_electronico: String,
+    val contrasena: String,
+    val puntos: Int = 0
 )
